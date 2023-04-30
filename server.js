4fdeed6fcd7f9607f019f959ac8443b002e41eb7
unknown <heitorcarrazzoni@gmail.com>
2023-04-30 16:19:24 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+require('./src/routes/index')(app);
+app.use(cors());
+app.use(express.json());
+app.listen(3333);
