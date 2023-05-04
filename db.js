6136db8564c1b16062f1447be72678ac8631aafd
unknown <heitorcarrazzoni@gmail.com>
2023-05-04 18:21:55 -0300

    Teste

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres'});
+const sequelize = new Sequelize('postgres://yvddudmtqsqwhz:5a33309c2192ecd4de0821bb28da72b7fdd0fc4a1531d22748affcdc9bdec212@ec2-34-197-91-131.compute-1.amazonaws.com:5432/db9aovo0ime2mr', {dialect: 'postgres'});
 
 module.exports = sequelize;
