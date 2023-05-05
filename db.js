77254d6ead6ed312cd8bf20ee1b93ea393a1dd29
unknown <heitorcarrazzoni@gmail.com>
2023-05-04 22:05:41 -0300

    Ultimo

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-sequelize = new Sequelize('postgres://yvddudmtqsqwhz:5a33309c2192ecd4de0821bb28da72b7fdd0fc4a1531d22748affcdc9bdec212@ec2-34-197-91-131.compute-1.amazonaws.com:5432/db9aovo0ime2mr', 
+sequelize = new Sequelize(process.env.DATABASE_URL, 
 {
     dialectOptions: {
       ssl: {
