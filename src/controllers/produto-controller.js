53ffc3c18f28160c07fd876cb1af4ef4f3b5411c
unknown <heitorcarrazzoni@gmail.com>
2023-05-04 22:50:01 -0300

    ultimo

--- src/controllers/produto-controller.js
+++ src/controllers/produto-controller.js
@@ -2,11 +2,13 @@ const produtos = require('../data/produtos.json');
 
 exports.post = (req, res, next) => {
    let id = req.body.id
-   if (id==null || id==undefined || produtos.includes(x=>x.id==id)) {
-      res.status(500).send("Erro - id não pode ser nulo ou já existe"); 
+   console.log(produtos.includes(x=>x.id===id))
+   if (id==null || id==undefined || produtos.find(x=>x.id==id)!=null) {
+    res.status(500).send("Erro - id não pode ser nulo ou já existe"); 
+    return
    }
-    produtos.push(req.body)
-    res.status(201).send();
+   produtos.push(req.body)
+   res.status(201).send();
  };
   
  exports.put = (req, res, next) => {
